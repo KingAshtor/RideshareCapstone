c0cb28a50f3783abb75a1d797e1d2666ade513f2
vovermiller <73202425+vovermiller@users.noreply.github.com>
2022-02-07 21:27:21 -0500

    Android Studio Project Creation
    Created android studio project and started the base of the application.

---
+++ CapstoneAndroid/app/src/main/java/com/example/ridesharecapstone/RidePage.java
@@ -0,0 +1,14 @@
+package com.example.ridesharecapstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RidePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_page);
+    }
+}
