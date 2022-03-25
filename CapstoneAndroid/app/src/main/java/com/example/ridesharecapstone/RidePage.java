8e36580e61012c9ecc4864c25b3e07cfe0f30093
KingAshtor <Ashtonsisson@gmail.com>
2022-03-24 20:15:51 -0400

    Request page and completion page finished (needs connection to api)

--- CapstoneAndroid/app/src/main/java/com/example/ridesharecapstone/RidePage.java
+++ CapstoneAndroid/app/src/main/java/com/example/ridesharecapstone/RidePage.java
@@ -1,22 +1,29 @@
 package com.example.ridesharecapstone;
 
-import static com.example.ridesharecapstone.api.retrofit.Api.API;
-import static com.example.ridesharecapstone.api.retrofit.Api.enqueue;
-import static com.example.ridesharecapstone.util.Hash.doubleHash;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-import com.example.ridesharecapstone.api.User;
-import com.example.ridesharecapstone.api.Ride;
+import static com.example.ridesharecapstone.api.retrofit.Api.API;
+import static com.example.ridesharecapstone.api.retrofit.Api.enqueue;
 
-import java.util.Arrays;
-import java.util.regex.Matcher;
+import com.example.ridesharecapstone.api.Route;
+import com.google.gson.JsonObject;
 
+import java.lang.reflect.Type;
+import java.util.Calendar;
+import java.util.Date;
+
+import retrofit2.Response;
+
+//implements added just for the time/date stuff, not needed for normal views
 public class RidePage extends AppCompatActivity {
 
     @Override
@@ -24,30 +31,77 @@ public class RidePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ride_page);
 
-        //assign values to everything on page
-        final Button submit = findViewById(R.id.submitBtn); //declares submit button as a button
+        //assign buttons
+        Button subBtn = findViewById(R.id.subBTN); //declares submit button as a button
 
-        final TextView date = findViewById(R.id.dateInput); //declares date field as a textView
-        final TextView time = findViewById(R.id.timeInput); //declares time field as a textView
-//        final TextView pickup = findViewById(R.id.pickupInput); //declares pickup field as a textView
-//        final TextView destination = findViewById(R.id.destinationInput); //declares destination field as a textView
+        //assign pickup fields
+        final TextView pickAddressLine1 = findViewById(R.id.pickAddressLine);
+        final TextView pickAddressLine2 = findViewById(R.id.pickAddressLine2);
+        final TextView pickCity = findViewById(R.id.pickCity);
+        final TextView pickState = findViewById(R.id.pickState);
+        final TextView pickZip = findViewById(R.id.pickZip);
 
-        //Submit button listener
-        submit.setOnClickListener(view -> {
-//            final Matcher pickupMatcher = ADDRESS_PATTERN.matcher(txt(pickup));
-//            final Matcher destinationMatcher = ADDRESS_PATTERN.matcher(txt(destination));
+        //assign destination fields
+        final TextView destAddressLine1 = findViewById(R.id.destAddressLine1);
+        final TextView destAddressLine2 = findViewById(R.id.destAddressLine2);
+        final TextView destCity = findViewById(R.id.destCity);
+        final TextView destState = findViewById(R.id.destState);
+        final TextView destZip = findViewById(R.id.destZip);
 
-            //Test form data
-            if (Arrays.asList(
-                    txt(date), txt(time)//, txt(pickup), txt(destination)
-            ).contains(""))
-                return; //First test to make sure no fields are blank, then tests to make sure destination and pickup are valid addresses
+        final AppCompatActivity context = this; //saves the current context for later use
+
+        //assign onClick listener to the submit button
+        subBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                enqueue(sendRequest(context, Route.class));
+                sendTheStuff();
+                toRequestComplete();
+            }
         });
-
-
     }
 
+    private void collectData() {
+    }
+
+    //used to get the text from a textview as a string
     private String txt(TextView view) {
         return view.getText().toString();
     }
+
+    //used to get the date from the date field
+    private Date dateTime(DatePicker datePicker, TimePicker timePicker) {
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year =  datePicker.getYear();
+        int hour = timePicker.getHour();
+        int minute = timePicker.getMinute();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day, hour, minute);
+
+        return calendar.getTime();
+    }
+
+//    private void sendRequest(Type body, Response<JsonObject> response){
+//
+//
+//        Toast.makeText(getBaseContext(), "Request Sent", Toast.LENGTH_SHORT).show();
+//
+//    }
+
+    private void sendTheStuff(){
+        API.addRoute(28,29, 8);
+        Toast.makeText(getBaseContext(), "Stuff sent?", Toast.LENGTH_SHORT).show();
+    }
+
+    //used to swap to the request complete activity
+    public void toRequestComplete(){
+        Intent intent = new Intent(this, RideRequestComplete.class);
+        startActivity(intent);
+    }
+
+
+
+
 }
