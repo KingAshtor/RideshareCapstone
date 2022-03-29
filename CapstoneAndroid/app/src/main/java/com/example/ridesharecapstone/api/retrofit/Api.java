6cffbdae82f1e5ad230cad986c2f03c6dae98fd1
masondkl <40154487+masondkl@users.noreply.github.com>
2022-03-29 08:21:43 -0400

    Fix errors with retrofit

--- CapstoneAndroid/app/src/main/java/com/example/ridesharecapstone/api/retrofit/Api.java
+++ CapstoneAndroid/app/src/main/java/com/example/ridesharecapstone/api/retrofit/Api.java
@@ -1,68 +1,180 @@
 package com.example.ridesharecapstone.api.retrofit;
 
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
 
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.sql.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiConsumer;
 
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Converter;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Query;
 
 public interface Api {
+    Converter<ResponseBody, Integer> INTEGER_CONVERTER =  value -> Integer.valueOf(value.string());
+    Converter.Factory INTEGER_FACTORY = new Converter.Factory() {
+        @Nullable
+        @Override
+        public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations, Retrofit retrofit) {
+            return INTEGER_CONVERTER;
+        }
+
+        @Nullable
+        @Override
+        public Converter<?, RequestBody> requestBodyConverter(Type type, Annotation[] parameterAnnotations, Annotation[] methodAnnotations, Retrofit retrofit) {
+            return super.requestBodyConverter(type, parameterAnnotations, methodAnnotations, retrofit);
+        }
+
+        @Nullable
+        @Override
+        public Converter<?, String> stringConverter(Type type, Annotation[] annotations, Retrofit retrofit) {
+            return super.stringConverter(type, annotations, retrofit);
+        }
+    };
+
     // Add api queries here
-    @GET("/api/roles/add/byEmail")
-    Call<JsonObject> addRole(@Query("email") String email, @Query("role") String role);
+    @POST("/api/roles/add/byEmail")
+    Call<Void> addRole(@Query("email") String email, @Query("role") String role);
 
     @GET("/api/user/byEmail")
     Call<JsonObject> getUser(@Query("email") String email);
 
-    @GET("/api/ride/addr/add")
-    Call<JsonObject> addAddr(@Query("line1") String line1, @Query("line2") String line2,
+    @POST("/api/ride/addr/add")
+    Call<Integer> addAddr(@Query("line1") String line1, @Query("line2") String line2,
                              @Query("city") String city, @Query("state") String state, @Query("zip") String zip);
+    @DELETE("/api/ride/addr/del")
+    Call<Void> delAddr(@Query("id") int id);
 
-    @GET("/api/ride/addr/del")
-    Call<JsonObject> delAddr(@Query("id") int id);
+    @PUT("/api/user/put")
+    Call<Void> putUser(@Body RequestBody body);
 
-    @GET("/api/user/put")
-    Call<JsonObject> putUser(@Body String body);
+    @POST("/api/ride/route/add")
+    Call<Integer> addRoute(@Query("from") int fromAddressID, @Query("to") int toAddressID,
+                              @Query("driver") int driverID);
 
-     // | Simplify Requests
-     // v
+    @POST("/api/ride/add")
+    Call<Integer> addRide(@Query("route") int routeID, @Query("rider") int rider,
+                             @Query("datetime") Date datetime);
+
+    // | Simplify Requests
+    // v
 
     Gson GSON = new Gson();
-    ExecutorService SERVICE = new ThreadPoolExecutor(1, 10, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
     Retrofit RETROFIT = new Retrofit.Builder()
-            .baseUrl("https://6fb7-146-168-217-8.ngrok.io")
+            /////////////////Link goes here ////////////////////////////
+            .baseUrl("https://b1bb-146-168-217-8.ngrok.io")
+            //////////////MUST BE HTTPS OR GOOGLE BLOCKS FOR SECURITY/////
             .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(INTEGER_FACTORY)
             .build();
     Api API = RETROFIT.create(Api.class);
-    BiConsumer<Void, Response<JsonObject>> DO_NOTHING = (_1, _2) -> { };
-    static <T> void enqueue(BiConsumer<T, Response<JsonObject>> consumer, AppCompatActivity context, Call<JsonObject> call, Class<T> type) {
-        SERVICE.submit(() -> call.enqueue(new Callback<JsonObject>() {
+
+    class ApiResponse<T> {
+        private Integer code;
+        private T body;
+        private Throwable err;
+
+        public boolean hasError() {
+            return err != null;
+        }
+
+        public void setCode(Integer code) {
+            this.code = code;
+        }
+
+        public void setBody(T body) {
+            this.body = body;
+        }
+
+        public void setErr(Throwable err) {
+            this.err = err;
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public T getBody() {
+            return body;
+        }
+
+        public Throwable getErr() {
+            return err;
+        }
+
+        @Override
+        public String toString() {
+            return "ApiResponse{" +
+                    "code=" + code +
+                    ", body=" + body +
+                    ", err=" + err +
+                    '}';
+        }
+    }
+
+    static <T> T fromJson(JsonObject body, Class<T> type) {
+        return GSON.fromJson(GSON.toJson(body), type);
+    }
+
+    static <T> ApiResponse<T> handle(Call<T> call, Class<T> type) {
+        final ApiResponse<T> apiResponse = new ApiResponse<>();
+        final AtomicBoolean ready = new AtomicBoolean(false);
+        call.enqueue(new Callback<T>() {
             @Override
-            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                if (response.code() != 200) return;
+            public void onResponse(Call<T> call, Response<T> response) {
+                System.out.println("pass: " + response.code());
+                apiResponse.setCode(response.code());
+                if (response.body() != null)
+                    apiResponse.setBody(response.body());
+                apiResponse.setErr(null);
+                ready.set(true);
+            }
+
+            @Override
+            public void onFailure(Call<T> call, Throwable throwable) {
+                System.out.println("fail");
+                apiResponse.setCode(null);
+                apiResponse.setBody(null);
+                apiResponse.setErr(throwable);
+                ready.set(true);
                 try {
-                    consumer.accept(GSON.fromJson(GSON.toJson(response.body()), type), response);
-                } catch (JsonSyntaxException ignored) {
-                    context.finish();
+                    throw(throwable);
+                } catch (Throwable e) {
+                    e.printStackTrace();
                 }
             }
-            @Override public void onFailure(Call<JsonObject> call, Throwable t) { }
-        }));
+        });
+        final long start = System.currentTimeMillis();
+        while(System.currentTimeMillis() - start < 10000 && !ready.get()) {
+            try { Thread.sleep(50); }
+            catch (InterruptedException ignored) { }
+            if (ready.get()) break;
+        }
+        return apiResponse;
     }
 }
